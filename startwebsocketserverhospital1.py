6a115912ad688a159d99b056e39ba9a4a29424c1
sacha <sdumay@novagen.tech>
2019-06-14 10:41:46 +0200

    rename and clean startwebsocketserver files

--- startwebsocketserverhospital1.py
+++ startwebsocketserverhospital1.py
@@ -8,8 +8,8 @@ from sklearn.preprocessing import StandardScaler
 hook = sy.TorchHook(torch, verbose=True)
 torch.manual_seed(1)
 
-configs = {"id":"hospital_2","host": "localhost", "hook": hook, "verbose": False,
-           "port": 8084}
+configs = {"id":"hospital_1","host": "localhost", "hook": hook, "verbose": False,
+           "port": 8083}
 
 async def show_all(worker):
     await asyncio.sleep(0)
@@ -17,8 +17,6 @@ async def show_all(worker):
         print("Objects:", worker._objects)
         await asyncio.sleep(2.0)
 
-x = torch.tensor([-1, 2.0]).tag("x")
-y = torch.tensor([-3, 4]).tag("y")
 from sklearn.datasets import load_breast_cancer
 data = load_breast_cancer()
 # Standardise data for input into Neural Net
@@ -27,9 +25,8 @@ transformed = scaler.fit_transform(data.data[:int(len(data.data)/2)])
 data_features = torch.tensor(transformed, requires_grad=True).tag("#train_features")
 data_target = torch.tensor(data.target[:int(len(data.data)/2)]).tag("train_target")
 
-
 worker = sy.workers.WebsocketServerWorker(
-    data=[x,y,data_features,data_target],
+    data=[data_features,data_target],
     **configs
 )
 
